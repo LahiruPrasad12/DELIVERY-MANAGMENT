a7473d4f72bf9191e17f70e91de7e5326c3b1ac9
Oshada1988 <lahirupr471@gmail.com>
2021-08-10 09:16:25 +0530

    create project environment

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+require("dotenv").config();
+
+
+
+
+/*---------------------------------set up server-------------------------------*/
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{console.log(`server started on PORT : ${PORT}`)});
+
+
+
+
+/*---------------------------------Connect to mongoDB-------------------------------*/
+const URL= process.env.MONGO_CONNECT;
+
+mongoose.connect(URL,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+});
+
+const connection =mongoose.connection;
+
+connection.once("open", ()=>{
+    console.log("mongoDB Connection successful")
+})
+
+
+
+
+/*---------------------------------set Routes-------------------------------*/
