001bb050a5c504525c8dcee36231b3f8694a54b9
Oshada1988 <lahirupr471@gmail.com>
2021-08-10 14:54:12 +0530

    create route for update product details from the database

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -33,6 +33,27 @@ router.post('/add',auth,async(req,res)=>{
 
 
 
+//This route used to update a product details
+router.put('/update/:id',auth,async(req,res)=>{
+    let productId = req.params.id;
+
+    const {name,description,quantity,userId} =req.body;
+ 
+    const updateProduct = {
+        name,description,quantity,userId
+     };
+
+     await Products.findByIdAndUpdate(productId,updateProduct).then(()=>{
+        res.status(200).send({status:"product updated"});
+    }).catch((err)=>{
+        res.status(500).json({errMsg:err});
+    })
+})
+
+
+
+
+
 //This route used to delete products from table
 router.delete('/remove/:id',auth,async(req,res)=>{
     let productId = req.params.id;
