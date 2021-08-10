e9a527ee221c931bfb38000088f89b0272c2e4b9
Oshada1988 <lahirupr471@gmail.com>
2021-08-10 12:12:07 +0530

    create route for log in user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,10 +6,6 @@ const jwt = require("jsonwebtoken");
 
 var mail = null;
 
-router.post("/", async (req,res)=>{
-    res.send("Test")
-})
-
 
 //This route is used to register a new user
 router.post('/register', async(req,res)=>{
@@ -43,7 +39,7 @@ router.post('/register', async(req,res)=>{
         const salt =  await bcrypt.genSalt();
         const hashPassword = await bcrypt.hash(password,salt);
 
-        mail = email
+        mail = email;
 
         //save new user accout to database
         const newUser = new User({
@@ -51,19 +47,91 @@ router.post('/register', async(req,res)=>{
         })
 
         await newUser.save().then((user)=>{
-            res.json(user)
+           
         }).catch((err)=>{
             res.json(err)
         })
 
 
+        //sign the tokrn
+        const token = jwt.sign({
+            user : newUser._id
+        },process.env.JWT_SECRET);
+
+        console.log(token);
 
 
-        
+        //send the token in the HTTP-only cookie
+        res.cookie("token",token,{
+            httpOnly : true
+        }).send();
+
+
+
     }catch(err){
         console.error(err);
         res.status(500).send(err);
     }
+})
+
+
+
+
+
+
+
+//This route for log in user
+router.post('/login',async(req,res)=>{
+    try{
+
+        //Get user enterd details
+        const {email,password} = req.body;
+
+         /*-----------------------validate details-------------------------*/
+        //chack user filed all required data
+        if(!email || !password)
+            return res.status(400).json({msg : "please enter all required fill"})
+
+        //check user enter mail is whether exsisting the database
+        var existingUser = await User.findOne({email});
+        if(!existingUser)
+            return res.status(400).send({msg : "invalid mail address"});
+
+        //check whether user enter password is correct
+        const passwordIsCorrect = await bcrypt.compare(password,existingUser.hashPassword);
+        if(!passwordIsCorrect)
+            return res.status(400).send({msg : "invalid password"});
+
+        mail = email;
+
+        //sign the token
+        const token = jwt.sign({
+            user : existingUser._id,
+            },process.env.JWT_SECRET);
+        
+        
+         //send the token in the HTTP-only cookie
+        res.cookie("token",token,{
+            httpOnly : true
+        }).send();
+
+
+    }catch(err){
+        console.log(err);
+        res.status(400).send(err);
+    }
+})
+
+
+
+
+
+//This route for log out user
+router.route("/logout").get(async(req,res)=>{
+    res.cookie("token","",{
+        httpOnly:true,
+        expires: new Date(0)
+    }).send();
 })
 
 module.exports = router;
